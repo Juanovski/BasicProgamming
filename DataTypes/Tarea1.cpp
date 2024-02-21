120b6c25542d081a8e73c150ee43536f941fd7ea
Juanovski <160760056+Juanovski@users.noreply.github.com>
2024-02-21 11:09:47 -0500

    Add files via upload

---
+++ DataTypes/Tarea1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main (int argc,char** argv) {
+  float f1 = 10e2;
+  double d1 = 12E4;
+  cout << f1 << "\n";
+  cout << d1 << endl;
+  return 0;
+}
