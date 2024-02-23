fb7fe77373707796777209c143a89c1492a790bb
Juanovski <160760056+Juanovski@users.noreply.github.com>
2024-02-23 18:36:48 -0500

    Add files via upload

---
+++ Operators C++/Arithmetic/Try it Division.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc,char** argv) {
+  int x = 8;
+  int y = 2;
+  cout << x / y;
+  return 0;
+}
+
---
+++ Operators C++/Arithmetic/Try it multiplicacion.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc,char** argv) {
+  int x = 9;
+  int y = 5;
+  cout << x * y;
+  return 0;
+}
+
+
