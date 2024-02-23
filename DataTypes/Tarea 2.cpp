737d7d829a323f8b90a87e2af02f92e48aeb87e8
Juanovski <160760056+Juanovski@users.noreply.github.com>
2024-02-23 17:12:24 -0500

    Add files via upload

---
+++ DataTypes/Tarea 2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main (int argc,char** argv) {
+  bool isCodingFun = true;
+  bool isFishTasty = false;
+  cout << isCodingFun << "\n";
+  cout << isFishTasty;
+  return 0;
+}
