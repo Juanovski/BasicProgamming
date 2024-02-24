12025507534c8012f420fbab057284db0964c5e7
Juanovski <160760056+Juanovski@users.noreply.github.com>
2024-02-23 20:44:37 -0500

    Add files via upload

---
+++ Operators C++/Assignment/Assignment Operators.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc,char** argv) {
+  int x = 10;
+  x += 5;
+  cout << x;
+  return 0;
+}
+
---
+++ Operators C++/Assignment/Try it ^=.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc,char** argv) {
+  int x = 4;
+  x ^= 5;
+  cout << x;
+  return 0;
+}
+
