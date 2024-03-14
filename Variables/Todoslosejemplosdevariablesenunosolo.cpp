ba26ae8322a553170dd5c44310ec821cbbdd3377
Juanovski <160760056+Juanovski@users.noreply.github.com>
2024-03-13 22:11:37 -0500

    Add files via upload

---
+++ Variables/Todoslosejemplosdevariablesenunosolo.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+/* run this program using the consolo pauser or add your own getch, system("pause") or input loop */
+
+int  main(int argc, char**argv) {
+	 
+    int myNum=15; // myNum is 15 
+	myNum=10;// Now myNum is 10 
+	cout << myNum<<endl; // outputs 10
+	
+	myNum= 5; //number whole
+	double myFloatNum =5.99; 
+
+	
+	char myLetter= 'j';// Character string myText="Hello"; // String (text )
+	bool myBoolean=true; //Boolean (true or false)
+	
+	int myAge= 17;
+	cout << "I am "<<myAge<<" years old"<<endl;
+	
+	
+	int x= 5;
+	int y= 6;
+	int sum = x+y;
+	cout <<sum<<endl;
+	
+	myNum=50;
+    cout<<myNum;
+
+	return 0; 
+	
+}
